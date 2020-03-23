37a455e87b8af67a4e30d49c415694849923c459
edgutterres <diasgt.eduardo@gmail.com>
2020-03-23 03:14:31 -0300

    Consertando cálculos do backend

--- api/api.py
+++ api/api.py
@@ -2,12 +2,14 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/calculate', methods=['GET','POST'])
+
+@app.route('/calculate', methods=['GET', 'POST'])
 def calculate():
-    teststring = request.json
-    number = 14
-    divList = []
-    for i in range(1, number+1):
-        if number%i == 0:
-            divList.append(i)
-    return {'playerName': teststring}
+    if request.method == 'GET':
+        number = int(request.args.get('number', None))
+        if number:
+            divList = [i for i in range(1, number+1) if number % i == 0]
+            isPrime = (True if len(divList)==2 else False)
+            
+            return {'divList': divList, 'isPrime': isPrime}
+        return "Nenhum número foi inserido"
